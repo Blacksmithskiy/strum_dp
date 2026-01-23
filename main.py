32c76b05249b294940e81fbee24ca2a62efe110b
Blacksmithskiy <blacksmithsky@gmail.com>
2026-01-23 06:26:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ NOISE_WORDS = ['вода', 'водоканал', 'труб', 'каналізац
 EMERGENCY_WORDS = ['екстрені', 'екстрене', 'скасовані графіки']
 
 genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel('gemini-pro-vision')
+model = genai.GenerativeModel('gemini-1.5-flash')
+
 
 
 
